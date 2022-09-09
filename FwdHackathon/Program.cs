bc5d9eaa26050b170f657c15ee156f696bcaf9b3
Anthony Lee <anthonyleehj@gmail.com>
2022-09-09 21:55:43 +0800

    Identity & mysql connection string

--- FwdHackathon/Program.cs
+++ FwdHackathon/Program.cs
@@ -1,4 +1,38 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using FwdHackathon.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("AppDbContextConnection") ?? throw new InvalidOperationException("Connection string 'AppDbContextConnection' not found.");
+
+var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+// Add MySQL DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+  string connectionString;
+  var serverVersion = new MySqlServerVersion(new Version(8, 0, 28));
+
+  if (env == "Production")
+  {
+    // If the production environment, use connection string in heroku config vars
+    var connUser = Environment.GetEnvironmentVariable("Username");
+    var connPass = Environment.GetEnvironmentVariable("Password");
+    var connHost = Environment.GetEnvironmentVariable("Host");
+    var connDb = Environment.GetEnvironmentVariable("Database");
+
+    connectionString = $"server={connHost};Uid={connUser};Pwd={connPass};Database={connDb}";
+  }
+  else
+  {
+    // If development environment, use connection string in appsettings config
+    connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+  }
+
+  options.UseMySql(connectionString, serverVersion);
+});
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AppDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -17,6 +51,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
