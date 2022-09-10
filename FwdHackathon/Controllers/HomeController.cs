a09bb547a0ca0725fec4c6a6e17571b797ec0a34
Anthony Lee <anthonyleehj@gmail.com>
2022-09-10 18:44:22 +0800

    File upload

--- FwdHackathon/Controllers/HomeController.cs
+++ FwdHackathon/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using FwdHackathon.Areas.Identity.Data;
+﻿using Firebase.Auth;
+using Firebase.Storage;
+using FwdHackathon.Areas.Identity.Data;
 using FwdHackathon.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -30,23 +32,23 @@ namespace FwdHackathon.Controllers
       });
 
       var jsonResponse = homeTimelineResult.Content;
-            var unQuotedString = jsonResponse.TrimStart('[').TrimEnd(']');
-            TrendsList model = JsonConvert.DeserializeObject<TrendsList>(unQuotedString);
+      var unQuotedString = jsonResponse.TrimStart('[').TrimEnd(']');
+      TrendsList model = JsonConvert.DeserializeObject<TrendsList>(unQuotedString);
 
-            int counter = 0;
-            model.trends = model.trends.Take(5).ToList();
-            foreach(Trend t in model.trends)
-            {
-                if (counter>4)
-                {
-                    model.trends.Remove(t);
-                }
-                else
-                {
-                    counter += 1;
-                }
-                
-            }
+      int counter = 0;
+      model.trends = model.trends.Take(5).ToList();
+      foreach (Trend t in model.trends)
+      {
+        if (counter > 4)
+        {
+          model.trends.Remove(t);
+        }
+        else
+        {
+          counter += 1;
+        }
+
+      }
 
       return View(model.trends);
     }
@@ -54,28 +56,67 @@ namespace FwdHackathon.Controllers
     public async Task<JsonResult> getTweets(string hashtag)
     {
 
-        var tweets = await _userClient.Execute.RequestAsync(request =>
-        {
-            request.Url = "https://api.twitter.com/1.1/search/tweets.json?q=%23" + hashtag;
-            request.HttpMethod = Tweetinvi.Models.HttpMethod.GET;
-        });
+      var tweets = await _userClient.Execute.RequestAsync(request =>
+      {
+        request.Url = "https://api.twitter.com/1.1/search/tweets.json?q=%23" + hashtag;
+        request.HttpMethod = Tweetinvi.Models.HttpMethod.GET;
+      });
 
-        var jsonResponse = tweets.Content;
-        var unQuotedString = jsonResponse.TrimStart('[').TrimEnd(']');
-           
-        return Json(unQuotedString);
+      var jsonResponse = tweets.Content;
+      var unQuotedString = jsonResponse.TrimStart('[').TrimEnd(']');
+
+      return Json(unQuotedString);
     }
 
-        public IActionResult Upload()
+    public IActionResult Upload()
     {
       return View();
+    }
+
+    [HttpPost]
+    [DisableRequestSizeLimit]
+    public async Task<IActionResult> Upload(IFormFile files)
+    {
+      //authentication
+      var auth = new FirebaseAuthProvider(new FirebaseConfig("AIzaSyDWm3HA3AqJQ3OTRlY3H6L_mxWhLmgKILU"));
+      var a = await auth.SignInWithEmailAndPasswordAsync("anthonyleehj@gmail.com", "abcd1234");
+
+      // Constructr FirebaseStorage, path to where you want to upload the file and Put it there
+      var task = new FirebaseStorage(
+          "fwd-hack-2022.appspot.com",
+           new FirebaseStorageOptions
+           {
+             AuthTokenAsyncFactory = () => Task.FromResult(a.FirebaseToken),
+             ThrowOnCancel = true,
+           })
+          .Child("data")
+          .Child("random")
+          .Child("file.csv")
+          .PutAsync(files.OpenReadStream());
+
+      // Track progress of the upload
+      task.Progress.ProgressChanged += (s, e) => Console.WriteLine($"Progress: {e.Percentage} %");
+
+      // await the task to wait until upload completes and get the download url
+      var downloadUrl = await task;
+
+      // load the csv file into memory
+      var data = files.OpenReadStream();
+
+      // create a dictionary of category : count
+      Dictionary<string, string> categories = new Dictionary<string, string>(); 
+
+      // pass the dictionary of category : count to the trends page
+
+      // redirect to trends page
+      return RedirectToAction(nameof(Index));
     }
 
     public IActionResult Trends()
     {
       return View();
     }
-        
+
     public IActionResult Privacy()
     {
       return View();
